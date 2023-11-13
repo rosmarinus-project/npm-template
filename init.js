e51a23a6d118b94945210fb173a54128310c66ab
JOJO <978940343@qq.com>
2023-11-13 13:23:42 +0800

    init

---
+++ init.js
@@ -0,0 +1,27 @@
+const { execSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+
+const projectRoot = __dirname;
+
+const projectName = process.argv[2];
+
+if (!projectName) {
+  throw new Error('Missing project name');
+}
+
+execSync('pnpm i', { cwd: projectRoot, stdio: 'inherit' });
+
+const KEY = 'npm-template';
+
+const FILES = ['.github/workflows/publish.yml', 'package.json', 'README.md'];
+
+function main() {
+  for (const file of FILES) {
+    const content = readFileSync(file, 'utf-8');
+    const newContent = content.replace(KEY, projectName);
+
+    writeFileSync(file, newContent);
+  }
+}
+
+main();
